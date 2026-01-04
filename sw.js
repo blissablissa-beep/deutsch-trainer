abde0d4e796ee3a08f14a3d57bf85387f7fdac9f
blissablissa-beep <blissablissa@gmail.com>
2026-01-04 11:20:20 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "dt-trainer-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE)
+            .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
